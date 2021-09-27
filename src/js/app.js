c3e6084e8f0206d32910e56a3703c8da70f41a8f
Muhammad Chaudhry <71554327+MuhammadC11@users.noreply.github.com>
2021-09-27 12:26:12 -0400

    push

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,86 @@
+import { DOMSelectors } from "./DOM";
 console.log("Connected");
 
 fetch("https://byabbe.se/on-this-day/5/30/events.json")
   .then((response) => response.json())
   .then((data) => console.log(data));
+
+const listen = function () {
+  DOMSelectors.searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    DOMSelectors.grid.innerHTML = "";
+    const searchParams = DOMSelectors.searchArea.value;
+    console.log(searchParams);
+    if (DOMSelectors.searchArea.value === "") {
+      alert("Please Input Something To Get Your Desired Results");
+      return;
+    }
+    const searchQuery = async function () {
+      try {
+        const response = await fetch(
+          `https://www.cheapshark.com/api/1.0/deals?storeID=1&title=${searchParams}`
+        );
+        const data = await response.json();
+        if (data.length === 0) {
+          alert("Whoops, looks like we couldn't find anything!");
+        }
+        data.forEach((games) => {
+          let storesArr = [];
+          const addStore = function () {
+            stores.forEach((element) => {
+              if (games.storeID.includes(element.id)) {
+                storesArr.push(element.name);
+                return storesArr;
+              }
+            });
+          };
+          addStore();
+
+          DOMSelectors.grid.insertAdjacentHTML(
+            "beforeend",
+            `<div class="games-card">
+              <div class="games-card-front">
+                <img
+                  src="${games.thumb}"
+                  alt=""
+                  class="poster"
+                />
+              </div>
+              <div class="games-card-back">
+                <h3 class="games-card-header">${games.title}</h3>
+                <div class="price-box">
+                  <p class="price">Get it now for:$ ${games.salePrice}</p>
+                  <p class="price">Original Price:$ ${games.normalPrice}</p>
+                 
+                </div>
+        
+                <div class="score-box">
+                  <p class="criticsTitle">Critics Ratings: ${
+                    games.metacriticScore
+                  }</p>
+                 
+                </div>
+        
+                <div class="savings-box">
+                  <p class="savings">${Math.round(games.savings)}% off</p>
+                 
+                </div>
+        
+                <div class="games-genres">
+                    From ${storesArr}
+                  </div>
+        
+              </div>
+            </div> `
+          );
+        });
+        console.log(data.length);
+      } catch (error) {
+        console.log(error);
+        alert("Oops, something bad happened");
+      }
+    };
+    searchQuery();
+  });
+};
+listen();
