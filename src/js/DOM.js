180d0e7ef4e0e64652c6ba3ee6857f6c47eabf55
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2021-09-27 22:45:08 -0400

    smthn

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -2,3 +2,4 @@ const DOMSelectors = {
   searchForm: document.getElementById("search-form"),
   searchArea: document.getElementById("search-area"),
 };
+export { DOMSelectors };
--- src/js/app.js
+++ src/js/app.js
@@ -8,7 +8,6 @@ fetch("https://byabbe.se/on-this-day/5/30/events.json")
 const listen = function () {
   DOMSelectors.searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
-    DOMSelectors.grid.innerHTML = "";
     const searchParams = DOMSelectors.searchArea.value;
     console.log(searchParams);
     if (DOMSelectors.searchArea.value === "") {
@@ -18,7 +17,7 @@ const listen = function () {
     const searchQuery = async function () {
       try {
         const response = await fetch(
-          `https://www.cheapshark.com/api/1.0/deals?storeID=1&title=${searchParams}`
+          `https://byabbe.se/on-this-day/${searchParams}/30/events.json`
         );
         const data = await response.json();
         if (data.length === 0) {
@@ -35,44 +34,6 @@ const listen = function () {
             });
           };
           addStore();
-
-          DOMSelectors.grid.insertAdjacentHTML(
-            "beforeend",
-            `<div class="games-card">
-              <div class="games-card-front">
-                <img
-                  src="${games.thumb}"
-                  alt=""
-                  class="poster"
-                />
-              </div>
-              <div class="games-card-back">
-                <h3 class="games-card-header">${games.title}</h3>
-                <div class="price-box">
-                  <p class="price">Get it now for:$ ${games.salePrice}</p>
-                  <p class="price">Original Price:$ ${games.normalPrice}</p>
-                 
-                </div>
-        
-                <div class="score-box">
-                  <p class="criticsTitle">Critics Ratings: ${
-                    games.metacriticScore
-                  }</p>
-                 
-                </div>
-        
-                <div class="savings-box">
-                  <p class="savings">${Math.round(games.savings)}% off</p>
-                 
-                </div>
-        
-                <div class="games-genres">
-                    From ${storesArr}
-                  </div>
-        
-              </div>
-            </div> `
-          );
         });
         console.log(data.length);
       } catch (error) {
