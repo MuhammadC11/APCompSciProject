a1b47a9b047a86291d8dec64de609b0cb101fdba
Muhammad Chaudhry <71554327+MuhammadC11@users.noreply.github.com>
2021-10-01 12:27:14 -0400

    push

---
+++ src/js/month.js
@@ -0,0 +1,88 @@
+const months = [
+  {
+    id: 1,
+    month: "january",
+  },
+  {
+    id: 2,
+    month: "february",
+  },
+  {
+    id: 3,
+    month: "march",
+  },
+  {
+    id: 4,
+    month: "april",
+  },
+  {
+    id: 5,
+    month: "may",
+  },
+  {
+    id: 6,
+    month: "june",
+  },
+  {
+    id: 7,
+    month: "july",
+  },
+  {
+    id: 8,
+    month: "august",
+  },
+  {
+    id: 9,
+    month: "september",
+  },
+  {
+    id: 10,
+    month: "october",
+  },
+  {
+    id: 11,
+    month: "november",
+  },
+  {
+    id: 12,
+    month: "december",
+  },
+];
+switch (months) {
+  case January:
+    number = "1";
+    break;
+  case February:
+    number = "2";
+    break;
+  case March:
+    number = "3";
+    break;
+  case April:
+    number = "4";
+    break;
+  case May:
+    number = "5";
+    break;
+  case June:
+    number = "6";
+    break;
+  case July:
+    number = "7";
+    break;
+  case August:
+    number = "8";
+    break;
+  case September:
+    number = "9";
+    break;
+  case October:
+    number = "10";
+    break;
+  case November:
+    number = "11";
+    break;
+  case December:
+    number = "12";
+    break;
+}
