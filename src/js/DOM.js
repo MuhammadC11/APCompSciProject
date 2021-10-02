3c6485ca45b3ca411d82ee19a4911411d14e9651
Muhammad Chaudhry <muhammad.chaudhry22@sitechhs.com>
2021-10-02 18:53:32 -0400

    push

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -2,5 +2,6 @@ const DOMSelectors = {
   searchForm: document.getElementById("search-form"),
   searchAreaDay: document.getElementById("search-area-day"),
   searchAreaMonth: document.getElementById("search-area-month"),
+  container: document.getElementById("container"),
 };
 export { DOMSelectors };
--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,20 @@
 import { DOMSelectors } from "./DOM";
 console.log("Connected");
 
-// fetch("https://byabbe.se/on-this-day/5/30/events.json")
-//   .then((response) => response.json())
-//   .then((data) => console.log(data));
+fetch(
+  "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=50&sortBy=Metacritic&pageSize=30&pageNumber=1"
+)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
 const listen = function () {
   DOMSelectors.searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
     const searchParamsDay = DOMSelectors.searchAreaDay.value;
     const searchParamsMonth = DOMSelectors.searchAreaMonth.value;
-    console.log(searchParamsDay);
     console.log(searchParamsMonth);
+    console.log(searchParamsDay);
+
     if (
       (DOMSelectors.searchAreaDay.value === "",
       DOMSelectors.searchAreaMonth.value === "")
@@ -29,9 +32,17 @@ const listen = function () {
         if (data.length === 0) {
           alert("Whoops, looks like we couldn't find anything!");
         }
-        data.forEach((history) => {
-          DOMSelectors;
-        });
+        // data.for((history) => {
+        //   DOMSelectors.container.insertAdjacentHTML(
+        //     "beforeend",
+        //     `<ul id="events-container">
+        //     Events on ${history.date}
+        //     <li class="event">
+        //     Hello
+        //     </li>
+        // </ul>`
+        //   );
+        // });
         console.log(data);
       } catch (error) {
         console.log(error);
